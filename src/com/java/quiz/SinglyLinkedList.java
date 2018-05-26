e69c4366839daba9e0bf7f03a5285665ff5dc81e
Fusion Solutions <solutions.fusion@gmail.com>
2018-05-26 14:00:00 +0530

    Added Singly Linked List Program...

---
+++ src/com/java/quiz/SinglyLinkedList.java
@@ -0,0 +1,18 @@
+package com.java.quiz;
+
+public class SinglyLinkedList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		LinkedList list = new LinkedList();
+		list.insertAtStart(1);
+		list.insertAtStart(2);
+		list.insertAtEnd(3);
+		list.insertAtEnd(4);		
+		list.display();
+	}
+
+}
